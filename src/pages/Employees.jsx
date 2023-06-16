a52debce5279419daa9887d9193af399b89ddf5b
ArnoldRamodike <riccoarnold23@gmail.com>
2023-06-16 14:32:50 +0200

    first commit

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { GridComponent, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, 
+  Search, Edit, Inject, ColumnsDirective, Toolbar} from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Employees = () => {
+  const toolbarOptions = ['Search'];
+
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, Edit, Search, Toolbar]} />
+
+      </GridComponent>
+    </div>
+  );
+};
+export default Employees;
